98002bf74a0dce1b9fe311528e01c6788cb5cc4e
DeathsGun <deathsgun@protonmail.com>
2022-02-09 16:35:47 +0100

    Abfahrt
    Signed-off-by: DeathsGun <deathsgun@protonmail.com>

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 1.1.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+  backend "s3" {
+    bucket = "deathsgun-tf"
+    key    = "krammatik/terraform.tfstate"
+    region = "eu-central-1"
+  }
+  required_version = ">=1.1.4"
 }
