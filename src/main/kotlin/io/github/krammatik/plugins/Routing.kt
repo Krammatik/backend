c16586913d3f368d450a2535d5fe5851d7765543
Oliver Schwietering <oliver.schwietering@group24.de>
2022-02-18 12:25:54 +0100

    feature/user-api

--- src/main/kotlin/io/github/krammatik/plugins/Routing.kt
+++ src/main/kotlin/io/github/krammatik/plugins/Routing.kt
@@ -47,6 +47,9 @@ fun Application.configureRouting() {
             exception<AuthorizationException> { cause ->
                 call.respond(HttpStatusCode.Forbidden, ErrorResponse(cause.message ?: ""))
             }
+            exception<NotFoundException> { cause ->
+                call.respond(HttpStatusCode.NotFound, ErrorResponse(cause.message ?: ""))
+            }
         }
         controller("/auth") { AuthenticationController(instance()) }
     }
@@ -55,3 +58,4 @@ fun Application.configureRouting() {
 class InvalidRequestException(override val message: String? = null) : RuntimeException()
 class AuthenticationException(override val message: String? = null) : RuntimeException()
 class AuthorizationException(override val message: String? = null) : RuntimeException()
+class NotFoundException(override val message: String? = null) : RuntimeException()
